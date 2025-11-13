d5e31704bb6e7e549faa9728f88ca2b702af8bfe
milch-meep <mila.chan666@gmail.com>
2025-11-13 10:03:47 +0300

    MiFirstKT

--- Assets/assets/srcipt/NewMonoBehaviourScript.cs
+++ Assets/assets/srcipt/NewMonoBehaviourScript.cs
@@ -1,6 +1,9 @@
-using UnityEngine;
-using TMPro;
+using System;
 using System.Linq;
+using System.Xml;
+using TMPro;
+using UnityEngine;
+using static UnityEngine.UI.Image;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public TMP_Text IText;
@@ -23,8 +26,22 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public void Loh()
     {
         IText.text = IText.text.Replace('a', '%');
-        IText.text = IText.text.();
+        IText.text = IText.text + "Lmao";
+        ReversLoha();
+    }
+    public void ReversLoha()
+    {
+        string originalText = IText.text;
+        string[] lines = originalText.Split('\n');
 
+        for (int i = 0; i < lines.Length; i++)
+        {
+            char[] chars = lines[i].ToCharArray();
+            System.Array.Reverse(chars);
+            lines[i] = new string(chars);
+        }
 
+        IText.text = string.Join("\n", lines);
     }
 }
+
